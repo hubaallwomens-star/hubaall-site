d1c2782c1df56ec3ec195fc227c6bb94ec0e3bc3
hubaallwomens-star <hubaallwomens@gmail.com>
2026-06-17 18:37:16 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Activity from './pages/Activity';
+import Leadership from './pages/Leadership';
+import Contact from './pages/Contact';
+import AgentHubaall from './components/AgentHubaall'; // ? ADD THIS
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/leadership" element={<Leadership />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+        <AgentHubaall /> {/* ? ADD THIS */}
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
